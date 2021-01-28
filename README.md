78749c7a4e7a5142c0bf011c36efa3fb61a43a0a
Chuan Zheng <zhengchuan000@gmail.com>
2021-01-27 19:24:39 -0500

    add article create and error messages alert

--- README.md
+++ README.md
@@ -13,10 +13,15 @@
 -  每一个route 对应一个Controller下的一个action, 也就是一个method
    每个在route.ru中的 controller(比如pages) 对应到 controller文件夹 下的pages_controller
 -  一个action可以去render一个页面, 也可以做其他事情, 如果要render一个页面的话, 页面名称就是
-   method_name.html.erb
+method_name.html.erb
+-  rails 的模板语法中的'%>' 不可以分开写 它是区分空格的.
 
 # 疑问
 - rails generate controller pages具体做了什么
 - 作业中提到的application server 和 web server的区别是什么
 - 搞清楚不同的GET POST请求的route怎么写
+- Ajax 和standard http的区别
+- rails routes --expanded后每一个route的prefix是什么意思, 和 article_controller中的article_path有什么关系, 和show.html.erb中的url又有什么关系
+- Article.new(params.require(:article).permit(:title, :description)) 什么意思
+- flash是什么时候触发的?
 
--- app/controllers/articles_controller.rb
+++ app/controllers/articles_controller.rb
@@ -1,6 +1,26 @@
 class ArticlesController < ApplicationController
     def show
-        byebug
-        @article = Article.find(params[:id])
+      @article = Article.find(params[:id])
+    end
+
+    def index
+      @articles = Article.all
+    end
+
+    def new
+      @article = Article.new
+    end
+
+    def create
+      # render plain: params[:article]
+      # cause error - > @article = Article.new(params[:article]) 
+      @article = Article.new(params.require(:article).permit(:title, :description))
+      if @article.save
+        flash[:notice] = "Article was created successfully."
+        redirect_to @article
+
+      else
+        render 'new'
+      end
     end
 end
